89344dbdc402c26bb238e4b01613252d22fdeeee
Akeemandela <easytwenty14@gmail.com>
2022-05-19 04:23:36 +0100

    User history file added

--- user_history.c
+++ user_history.c
@@ -6,24 +6,24 @@
  */
 int history(char *input)
 {
-	char *filename = ".simple_shell_history";
-	ssize_t fd, w;
-	int len = 0;
+char *filename = ".simple_shell_history";
+ssize_t fd, w;
+int len = 0;
 
-	if (!filename)
-		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR | O_APPEND, 00600);
-	if (fd < 0)
-		return (-1);
-	if (input)
-	{
-		while (input[len])
-			len++;
-		w = write(fd, input, len);
-		if (w < 0)
-			return (-1);
-	}
-	return (1);
+if (!filename)
+return (-1);
+fd = open(filename, O_CREAT | O_RDWR | O_APPEND, 00600);
+if (fd < 0)
+return (-1);
+if (input)
+{
+while (input[len])
+len++;
+w = write(fd, input, len);
+if (w < 0)
+return (-1);
+}
+return (1);
 }
 /**
  * free_env - Free Enviroment Variable Array
@@ -32,10 +32,10 @@ int history(char *input)
  */
 void free_env(char **env)
 {
-	int i;
+int i;
 
-	for (i = 0; env[i]; i++)
-	{
-		free(env[i]);
-	}
+for (i = 0; env[i]; i++)
+{
+free(env[i]);
+}
 }
