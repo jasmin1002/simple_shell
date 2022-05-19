920d9186d042147fe5efface8e4a2837c29f7334
Akeemandela <easytwenty14@gmail.com>
2022-05-19 05:02:39 +0100

    Parsing file added

--- parser.c
+++ parser.c
@@ -7,26 +7,26 @@
  */
 char **parse_cmd(char *input)
 {
-	char **tokens;
-	char *token;
-	int i, buffsize = BUFSIZE;
+char **tokens;
+char *token;
+int i, buffsize = BUFSIZE;
 
-	if (input == NULL)
-		return (NULL);
-	tokens = malloc(sizeof(char *) * buffsize);
-	if (!tokens)
-	{
-		perror("hsh");
-		return (NULL);
-	}
+if (input == NULL)
+return (NULL);
+tokens = malloc(sizeof(char *) * buffsize);
+if (!tokens)
+{
+perror("hsh");
+return (NULL);
+}
 
-	token = _strtok(input, "\n ");
-	for (i = 0; token; i++)
-	{
-		tokens[i] = token;
-		token = _strtok(NULL, "\n ");
-	}
-	tokens[i] = NULL;
+token = _strtok(input, "\n ");
+for (i = 0; token; i++)
+{
+tokens[i] = token;
+token = _strtok(NULL, "\n ");
+}
+tokens[i] = NULL;
 
-	return (tokens);
+return (tokens);
 }
