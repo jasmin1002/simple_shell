66b1a374ee0725343355cb56554ecc95fecb5595
Akeemandela <easytwenty14@gmail.com>
2022-05-19 04:54:18 +0100

    String function part 1 added

--- string_1.c
+++ string_1.c
@@ -10,11 +10,11 @@ char *_strcpy(char *dest, char *src)
 int i;
 
 i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
+while (src[i])
+{
+dest[i] = src[i];
+i++;
+}
 dest[i] = '\0';
 return (dest);
 }
@@ -26,21 +26,21 @@ return (dest);
  */
 char *_strcat(char *dest, char *src)
 {
-	char *s = dest;
+char *s = dest;
 
-	while (*dest != '\0')
-	{
-		dest++;
-	}
+while (*dest != '\0')
+{
+dest++;
+}
 
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (s);
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (s);
 }
 /**
  * _strchr - Locate Charactere In String
@@ -51,13 +51,15 @@ char *_strcat(char *dest, char *src)
 char *_strchr(char *s, char c)
 {
 
-	do		{
+do		
+{
 
-		if (*s == c)
-			{
-			break;
-			}
-		}	while (*s++);
+if (*s == c)
+{
+break;
+}
+}	
+while (*s++);
 
 return (s);
 }
@@ -71,18 +73,18 @@ return (s);
  */
 int _strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+size_t i;
 
-	if (s1 == NULL)
-		return (-1);
-	for (i = 0; i < n && s2[i]; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			return (1);
-		}
-	}
-	return (0);
+if (s1 == NULL)
+return (-1);
+for (i = 0; i < n && s2[i]; i++)
+{
+if (s1[i] != s2[i])
+{
+return (1);
+}
+}
+return (0);
 }
 /**
  * _strdup - Duplicate A String
@@ -91,20 +93,20 @@ int _strncmp(const char *s1, const char *s2, size_t n)
  */
 char *_strdup(char *str)
 {
-	size_t len, i;
-	char *str2;
+size_t len, i;
+char *str2;
 
-	len = _strlen(str);
-	str2 = malloc(sizeof(char) * (len + 1));
-	if (!str2)
-	{
-		return (NULL);
-	}
+len = _strlen(str);
+str2 = malloc(sizeof(char) * (len + 1));
+if (!str2)
+{
+return (NULL);
+}
 
-	for (i = 0; i <= len; i++)
-	{
-		str2[i] = str[i];
-	}
+for (i = 0; i <= len; i++)
+{
+str2[i] = str[i];
+}
 
-	return (str2);
+return (str2);
 }
