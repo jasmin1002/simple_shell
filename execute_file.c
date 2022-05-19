6e1cde8b9df4b47ca8be9a63ca2dd24731cc1e6a
Akeemandela <easytwenty14@gmail.com>
2022-05-19 05:14:57 +0100

    Execute file function added

--- execute_file.c
+++ execute_file.c
@@ -8,26 +8,26 @@
 
 void read_file(char *filename, char **argv)
 {
-	FILE *fp;
-	char *line = NULL;
-	size_t len = 0;
-	int counter = 0;
+FILE *fp;
+char *line = NULL;
+size_t len = 0;
+int counter = 0;
 
-	fp = fopen(filename, "r");
-	if (fp == NULL)
-	{
-		exit(EXIT_FAILURE);
-	}
-	while ((getline(&line, &len, fp)) != -1)
-	{
-		counter++;
-		treat_file(line, counter, fp, argv);
+fp = fopen(filename, "r");
+if (fp == NULL)
+{
+exit(EXIT_FAILURE);
+}
+while ((getline(&line, &len, fp)) != -1)
+{
+counter++;
+treat_file(line, counter, fp, argv);
 
-	}
-	if (line)
-		free(line);
-	fclose(fp);
-	exit(0);
+}
+if (line)
+free(line);
+fclose(fp);
+exit(0);
 }
 /**
  * treat_file - PARSE Check Command Fork Wait Excute in Line of File
@@ -39,25 +39,25 @@ void read_file(char *filename, char **argv)
  */
 void treat_file(char *line, int counter, FILE *fp, char **argv)
 {
-	char **cmd;
-	int st = 0;
+char **cmd;
+int st = 0;
 
-	cmd = parse_cmd(line);
+cmd = parse_cmd(line);
 
-		if (_strncmp(cmd[0], "exit", 4) == 0)
-		{
-			exit_bul_for_file(cmd, line, fp);
-		}
-		else if (check_builtin(cmd) == 0)
-		{
-			st = handle_builtin(cmd, st);
-			free(cmd);
-		}
-		else
-		{
-			st = check_cmd(cmd, line, counter, argv);
-			free(cmd);
-		}
+if (_strncmp(cmd[0], "exit", 4) == 0)
+{
+exit_bul_for_file(cmd, line, fp);
+}
+else if (check_builtin(cmd) == 0)
+{
+st = handle_builtin(cmd, st);
+free(cmd);
+}
+else
+{
+st = check_cmd(cmd, line, counter, argv);
+free(cmd);
+}
 }
 /**
  * exit_bul_for_file - Exit Shell Case Of File
@@ -68,28 +68,27 @@ void treat_file(char *line, int counter, FILE *fp, char **argv)
  */
 void exit_bul_for_file(char **cmd, char *line, FILE *fd)
 {
-	int statue, i = 0;
+int statue, i = 0;
 
-	if (cmd[1] == NULL)
-	{
-		free(line);
-		free(cmd);
-		fclose(fd);
-		exit(errno);
-	}
-	while (cmd[1][i])
-	{
-		if (_isalpha(cmd[1][i++]) < 0)
-		{
-			perror("illegal number");
-		}
-	}
-	statue = _atoi(cmd[1]);
-	free(line);
-	free(cmd);
-	fclose(fd);
-	exit(statue);
-
+if (cmd[1] == NULL)
+{
+free(line);
+free(cmd);
+fclose(fd);
+exit(errno);
+}
+while (cmd[1][i])
+{
+if (_isalpha(cmd[1][i++]) < 0)
+{
+perror("illegal number");
+}
+}
+statue = _atoi(cmd[1]);
+free(line);
+free(cmd);
+fclose(fd);
+exit(statue);
 
 
 }
