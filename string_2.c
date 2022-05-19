e36297f5d4de21592203d3225b10c7265d20c0d5
Akeemandela <easytwenty14@gmail.com>
2022-05-19 04:49:04 +0100

    String function part 2 added

--- string_2.c
+++ string_2.c
@@ -11,21 +11,21 @@ int cmp = 0, i, len1, len2;
 len1 = _strlen(s1);
 len2 = _strlen(s2);
 
-	if (s1 == NULL || s2 == NULL)
-		return (1);
-	if (len1 != len2)
-		return (1);
-	for (i = 0; s1[i]; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			cmp = s1[i] - s2[i];
-			break;
-		}
-		else
-			continue;
-	}
-	return (cmp);
+if (s1 == NULL || s2 == NULL)
+return (1);
+if (len1 != len2)
+return (1);
+for (i = 0; s1[i]; i++)
+{
+if (s1[i] != s2[i])
+{
+cmp = s1[i] - s2[i];
+break;
+}
+else
+continue;
+}
+return (cmp);
 }
 /**
  * _isalpha - Check if Alphabtic
@@ -50,24 +50,24 @@ return (0);
  */
 char *_itoa(unsigned int n)
 {
-	int len = 0, i = 0;
-	char *s;
+int len = 0, i = 0;
+char *s;
 
-	len = intlen(n);
-	s = malloc(len + 1);
-	if (!s)
-		return (NULL);
-	*s = '\0';
-	while (n / 10)
-	{
-		s[i] = (n % 10) + '0';
-		n /= 10;
-		i++;
-	}
-	s[i] = (n % 10) + '0';
-	array_rev(s, len);
-	s[i + 1] = '\0';
-	return (s);
+len = intlen(n);
+s = malloc(len + 1);
+if (!s)
+return (NULL);
+*s = '\0';
+while (n / 10)
+{
+s[i] = (n % 10) + '0';
+n /= 10;
+i++;
+}
+s[i] = (n % 10) + '0';
+array_rev(s, len);
+s[i + 1] = '\0';
+return (s);
 }
 /**
  *  array_rev - Reverse Array
@@ -77,15 +77,15 @@ char *_itoa(unsigned int n)
  */
 void array_rev(char *arr, int len)
 {
-	int i;
-	char tmp;
+int i;
+char tmp;
 
-	for (i = 0; i < (len / 2); i++)
-	{
-		tmp = arr[i];
-		arr[i] = arr[(len - 1) - i];
-		arr[(len - 1) - i] = tmp;
-	}
+for (i = 0; i < (len / 2); i++)
+{
+tmp = arr[i];
+arr[i] = arr[(len - 1) - i];
+arr[(len - 1) - i] = tmp;
+}
 }
 /**
  * intlen - Determine Length Of Int
@@ -94,12 +94,12 @@ void array_rev(char *arr, int len)
  */
 int intlen(int num)
 {
-	int len = 0;
+int len = 0;
 
-	while (num != 0)
-	{
-		len++;
-		num /= 10;
-	}
-	return (len);
+while (num != 0)
+{
+len++;
+num /= 10;
+}
+`return (len);
 }
